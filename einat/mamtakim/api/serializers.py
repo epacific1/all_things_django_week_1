f3d1df3b88af72b7e3e42724f9952194b2475a0d
Einat Pacifici <epacific@redhat.com>
2024-08-11 12:58:03 +0300

    Delete einat directory
    Deleting directory einat from main.

--- einat/mamtakim/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from mamtakim_store.models import Mamtak
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mamtak
-        fields = '__all__'
--- einat/mamtakim/mamtakim_store/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MamtakimStoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mamtakim_store"
